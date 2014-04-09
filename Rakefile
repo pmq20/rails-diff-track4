ec66d9cfd75868e14fbf52b89c0003e35e240f25
P.S.V.R <pmq2001@gmail.com>
2014-04-09 16:22:31 +0800

    4.0.4 -> 4.1.0

--- Rakefile
+++ Rakefile
@@ -3,4 +3,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-RailsDiffTrack4::Application.load_tasks
+Rails.application.load_tasks
--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-RailsDiffTrack4::Application.initialize!
+Rails.application.initialize!
