9ba0354adf62982d2af7a33f1fc884b9273f0a65
P.S.V.R <pmq2001@gmail.com>
2014-04-09 16:00:48 +0800

    4.0.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsDiffTrack4::Application.config.secret_key_base = 'f1bf5baea444174372f0543e886d529d52881bcc7fbba72990138f6122d88c6fa46de519b2ef6a70a97132b3c8468c8cd6b30fcef0879ede248a494d501d7774'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsDiffTrack4::Application.config.session_store :cookie_store, key: '_rails-diff-track4_session'
